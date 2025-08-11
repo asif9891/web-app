7fd41eadc39dd76342f8542459964c8161f00b77
asif9891 <asif.elec786@gmail.com>
2025-08-12 01:10:09 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const port = 3000
+const app = express
+
+app.listen(port,(err)=>{
+    if(err){
+        console,log(`server throw error ${port}`)
+        return
+    }
+    console.log(`server listen on port ${port}`)
+})
