37af4715c4121dd1ee21a342bcd78d234909eb05
asif9891 <asif.elec786@gmail.com>
2025-08-12 12:12:35 +0530

    Error resolve

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const port = 3000
 const app = express
 
-app.listen(port,(err)=>{
+app.listen(port,function(err){
     if(err){
         console,log(`server throw error ${port}`)
         return
