ef5a1cb79a58e1074a0f0c71953833a4074addab
asif9891 <asif.elec786@gmail.com>
2025-08-12 19:57:47 +0530

    First route is created

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const { route } = require('./routes');
 const port = 3000
 const app = express()
+
+
+app.use(express.urlencoded());
+app.use('/',require('./routes'));
 
 app.listen(port,function(err){
     if(err){
